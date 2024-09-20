5818374e098ecfa3bf0ccb12d67d627ab784ad5e
Tömöri Péter <tomoripeti2003@gmail.com>
2024-09-20 10:42:09 +0200

    Final version catalog

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,18 +1,4 @@
 
-val picoCliVersion = "4.7.6"
-val slf4jVersion = "1.7.36"
-val log4jVersion = "2.23.1"
-val junitVersion = "5.10.0"
-
 plugins {
     id("hu.bme.mit.ase.shingler.gradle.java")
-}
-
-dependencies {
-    implementation(libs.picocli)
-    implementation(libs.slf4j.api)
-    implementation(libs.slf4j.log4j.impl)
-    implementation(libs.slf4j.logback.impl)
-    implementation(libs.junit.jupiter.core)
-    implementation(libs.junit.jupiter.engine)
 }
--- logic/build.gradle.kts
+++ logic/build.gradle.kts
@@ -1,9 +1,4 @@
 
-val picoCliVersion = "4.7.6"
-val slf4jVersion = "1.7.36"
-val log4jVersion = "2.23.1"
-val junitVersion = "5.10.0"
-
 plugins {
     id("hu.bme.mit.ase.shingler.gradle.java")
 }
@@ -13,14 +8,10 @@ dependencies {
     //subproject dependency
     implementation(project(":lib"))
 
-    implementation("org.slf4j:slf4j-api:$slf4jVersion")
-    implementation("info.picocli:picocli:$picoCliVersion")
-
-    runtimeOnly("org.apache.logging.log4j:log4j-slf4j-impl:$log4jVersion")
-
-    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
-
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
-
+    implementation(libs.slf4j.api)
+    implementation(libs.picocli)
+    implementation(libs.slf4j.log4j.impl)
+    implementation(libs.junit.jupiter.core)
+    implementation(libs.junit.jupiter.engine)
 }
 
